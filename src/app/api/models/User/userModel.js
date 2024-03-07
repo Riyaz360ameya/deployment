89e6f561e212bb2065b6e2794969d5b581bb0d98
Riyaz360ameya <riyaz@ameya360.in>
2024-03-07 12:35:08 +0530

    added new field  user model for sending mail to client

--- src/app/api/models/User/userModel.js
+++ src/app/api/models/User/userModel.js
@@ -28,6 +28,10 @@ const userSchema = new mongoose.Schema({
       type: Boolean,
       default: false
    },
+   isFilesVerified:{
+      type: Boolean,
+      default: false
+   },
    isAdmin: {
       type: Boolean,
       default: false
