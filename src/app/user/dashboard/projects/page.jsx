900854fe638ed8f4d7a4508ce92a6bb04269f10e
Riyaz360ameya <riyaz@ameya360.in>
2024-03-21 23:16:48 +0530

    client table modified with search bar option ui

--- src/app/user/dashboard/projects/page.jsx
+++ src/app/user/dashboard/projects/page.jsx
@@ -28,7 +28,7 @@ function Projects() {
         setLoading(true);
         try {
             const { data } = await userProjects();
-            console.log(data,'-----------user project------------')
+            console.log(data, '-----------user project------------')
             const NewProjects = data.projectsInformation.NewProjects;
             dispatch(userNewProjects(NewProjects));
             const onGoingProjects = data.projectsInformation.onGoingProjects;
@@ -82,116 +82,99 @@ function Projects() {
                 </div>
             ) : (
                 <>
-                    <div className='p-2 h-full overflow-hidden overflow-y-scroll w-full overflow-x-hidden'>
+                    <div className='p-2 h-full overflow-hidden  w-full overflow-x-hidden'>
                         <div>
                             <h1 className='text-xl p-2 flex justify-center items-center shadow-lg bg-gray-200'>Your Projects Details</h1>
                             <div className='flex items-center justify-between'>
-                                {/* Pagination */}
+                            <div className='flex gap-4 ml-2 py-4 sticky top-0'>
+                                    <div onClick={() => handleData("New")} className={`py-2 px-8  ${position === "New" && "bg-indigo-200"}  hover:bg-indigo-100 text-indigo-700 rounded-full relative shadow-xl shadow-cyan-500/50 cursor-pointer`}>
+                                        <p className='font-bold '>New</p>
+                                    </div>
+                                    <div onClick={() => handleData("OnGoing")} className={`py-2 px-8  ${position === "OnGoing" && "bg-indigo-200"} hover:bg-indigo-100 text-indigo-700 rounded-full shadow-xl shadow-cyan-500/50 cursor-pointer`}>
+                                        <p className='font-bold shadow'>OnGoing</p>
+                                    </div>
+                                    <div onClick={() => handleData("Completed")} className={`py-2 px-8  ${position === "Completed" && "bg-indigo-200"} hover:bg-indigo-100 text-indigo-700 rounded-full shadow-xl shadow-cyan-500/50 cursor-pointer`}>
+                                        <p className='font-bold shadow'>Completed</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div className='border shadow mt-4'>
-                            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+
+                        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                            <div class="pb-4 px-4 bg-white dark:bg-gray-900 py-5">
+                                <label for="table-search" class="sr-only">Search</label>
+                                <div class="relative mb-3">
+                                    <div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
+                                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                                        </svg>
+                                    </div>
+                                    <input type="text" id="table-search" class=" block pt-2 pb-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for items" />
+                                </div>
+                            </div>
+                            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                     <tr>
-                                        <th scope="col" className="p-4">
-                                            <div className="flex items-center">
-                                                <input id="checkbox-all-search" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                                                <label htmlFor="checkbox-all-search" className="sr-only">checkbox</label>
+                                        <th scope="col" class="p-4">
+                                            <div class="flex items-center">
+                                                <input id="checkbox-all-search" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                                <label for="checkbox-all-search" class="sr-only">checkbox</label>
                                             </div>
                                         </th>
-                                      
-                                        <th scope="col" className="px-6 py-3">
+                                        <th scope="col" class="px-6 py-3">
                                             Project Name
                                         </th>
-                                        <th scope="col" className="px-6 py-3">
+                                        <th scope="col" class="px-6 py-3">
                                             Project Id
                                         </th>
-                                        <th scope="col" className="px-6 py-3">
+                                        <th scope="col" class="px-6 py-3">
                                             Project Type
                                         </th>
-                                        <th scope="col" className="px-6 py-3">
+                                        <th scope="col" class="px-6 py-3">
                                             Description
                                         </th>
-                                        <th scope="col" className="px-6 py-3">
+                                        <th scope="col" class="px-6 py-3">
                                             Project Usp
                                         </th>
-                                        <th scope="col" className="px-6 py-3">
+                                        <th scope="col" class="px-6 py-3">
                                             Reached On
                                         </th>
-                                        {/* <th scope="col" className="px-6 py-3">
-                                            Status
-                                        </th> */}
-
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {projects.length === 0 ? (
-                                        <tr className="mt-10 text-center border shadow-xl">
-                                            <td colSpan="7" className='text-2xl text-blue-600'>No Projects</td>
-                                        </tr>
-                                    ) : (
-                                        currentProjects.map((item, i) => (
-                                            <tr key={i} className='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'>
-                                                <td className="w-4 p-4">
-                                                    <div className="flex items-center">
-                                                        <input id={`checkbox-table-search-${i}`} type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                                                        <label htmlFor={`checkbox-table-search-${i}`} className="sr-only">checkbox</label>
-                                                    </div>
-                                                </td>
-                                               
-                                                <td className="px-6 py-4">
-                                                    {item.ProjectId.projectInfo.projectDetails.projectName}
-                                                </td>
-                                                <td className="px-6 py-4">
+                                    {currentProjects.map((item, i) => (
+                                        <tr key={i} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                            <td className="w-4 p-4">
+                                                <div className="flex items-center">
+                                                    <input id={`checkbox-table-search-${i}`} type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                                    <label htmlFor={`checkbox-table-search-${i}`} className="sr-only">checkbox</label>
+                                                </div>
+                                            </td>
+                                            <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                {item.ProjectId.projectInfo.projectDetails.projectName}
+                                            </td>
+                                            <td className="px-6 py-4">
                                                 {item.ProjectId.ProjectUniqId}
-                                                </td>
-                                                <td className="px-6 py-4">
+                                            </td>
+                                            <td className="px-6 py-4">
                                                 {item.ProjectId.projectInfo.projectDetails.projectType}
-                                                </td>
-                                               
-                                                <td className="px-6 py-4">
-                                                    {item.ProjectId.projectInfo.projectDetails.projectDes}
-                                                </td>
-                                                <td className="px-6 py-4">
-                                                    {item.ProjectId.projectInfo.projectDetails.projectUSP}
-                                                </td>
-                                                
-                                                
-                                                <td>{item.ProjectId.createdAt
-
-}</td>
-
-                                                {/* {
-                                                    position == "Completed" && <td className='font-bold bg-green-700'>{dateConverter(item.leadTaskCompletedDate)}</td>
-                                                }
-                                                {item.projectVerified ?
-                                                    <td>
-                                                        {
-                                                            position !== "Completed" ?
-                                                                item.payment === "Payment is Done" ?
-                                                                    <button className='px-3 py-1 text-white bg-blue-600 rounded' onClick={() => handleAssign({ projectId: item.projectId._id, itemId: item._id })} >Assign Task to</button>
-                                                                    :
-                                                                    item.status === "Assigned" ?
-                                                                        <>
-                                                                            <button className='px-3 text-white bg-blue-600 rounded'>E</button>
-                                                                            <button className='px-3 ml-2 text-white bg-red-600 rounded'>D</button>
-                                                                        </>
-                                                                        :
-                                                                        item.status === "Completed" ? <button className='px-3 py-1 text-white bg-blue-600 rounded' onClick={() => handleUpdate({ projectId: item.projectId._id, itemId: item._id, index: i })} >Update</button>
-
-                                                                            : <p className='text-red-600'>
-                                                                                Not Payed
-                                                                            </p>
-                                                                : ''
-                                                        }
-                                                    </td> : <td className='text-red-500 font-bold'>"Not verified"</td>
-                                                } */}
-                                            </tr>
-                                        ))
-                                    )}
+                                            </td>
+                                            <td className="px-6 py-4">
+                                                {item.ProjectId.projectInfo.projectDetails.projectDes}
+                                            </td>
+                                            <td className="px-6 py-4">
+                                                {item.ProjectId.projectInfo.projectDetails.projectUSP}
+                                            </td>
+                                            <td className="px-6 py-4">
+                                                {item.ProjectId.createdAt}
+                                            </td>
+                                        </tr>
+                                    ))}
                                 </tbody>
+
                             </table>
                         </div>
+                     
                     </div>
                     {
                         view ? <DataView setView={setView} allData={allData} /> : ''
@@ -290,17 +273,17 @@ export default Projects;
 //                         <div>
 //                             <h1 className='text-xl p-2 flex justify-center items-center shadow-lg bg-gray-200'>Your Projects Details</h1>
 //                             <div className='flex items-center justify-between'>
-//                                 <div className='flex gap-4 ml-2 py-4 sticky top-0'>
-//                                     <div onClick={() => handleData("New")} className={`py-2 px-8  ${position === "New" && "bg-indigo-200"}  hover:bg-indigo-100 text-indigo-700 rounded-full relative shadow-xl shadow-cyan-500/50 cursor-pointer`}>
-//                                         <p className='font-bold '>New</p>
-//                                     </div>
-//                                     <div onClick={() => handleData("OnGoing")} className={`py-2 px-8  ${position === "OnGoing" && "bg-indigo-200"} hover:bg-indigo-100 text-indigo-700 rounded-full shadow-xl shadow-cyan-500/50 cursor-pointer`}>
-//                                         <p className='font-bold shadow'>OnGoing</p>
-//                                     </div>
-//                                     <div onClick={() => handleData("Completed")} className={`py-2 px-8  ${position === "Completed" && "bg-indigo-200"} hover:bg-indigo-100 text-indigo-700 rounded-full shadow-xl shadow-cyan-500/50 cursor-pointer`}>
-//                                         <p className='font-bold shadow'>Completed</p>
-//                                     </div>
-//                                 </div>
+                                // <div className='flex gap-4 ml-2 py-4 sticky top-0'>
+                                //     <div onClick={() => handleData("New")} className={`py-2 px-8  ${position === "New" && "bg-indigo-200"}  hover:bg-indigo-100 text-indigo-700 rounded-full relative shadow-xl shadow-cyan-500/50 cursor-pointer`}>
+                                //         <p className='font-bold '>New</p>
+                                //     </div>
+                                //     <div onClick={() => handleData("OnGoing")} className={`py-2 px-8  ${position === "OnGoing" && "bg-indigo-200"} hover:bg-indigo-100 text-indigo-700 rounded-full shadow-xl shadow-cyan-500/50 cursor-pointer`}>
+                                //         <p className='font-bold shadow'>OnGoing</p>
+                                //     </div>
+                                //     <div onClick={() => handleData("Completed")} className={`py-2 px-8  ${position === "Completed" && "bg-indigo-200"} hover:bg-indigo-100 text-indigo-700 rounded-full shadow-xl shadow-cyan-500/50 cursor-pointer`}>
+                                //         <p className='font-bold shadow'>Completed</p>
+                                //     </div>
+                                // </div>
 //                                 <div className="">
 //                                     {Array.from({ length: Math.ceil(projects.length / projectsPerPage) }).map((_, index) => (
 //                                         <button
