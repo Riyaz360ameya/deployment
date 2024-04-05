6359e3ac55e7f2bf052d848b197e2dfc4fb25cd2
Sujith <sujith@ameya360.in>
2024-04-05 18:50:26 +0530

    changed the auth module header part as per next configuration

--- src/app/api/middleware/authMiddleware.js
+++ src/app/api/middleware/authMiddleware.js
@@ -1,35 +1,78 @@
-
-"use server"
 import jwt from 'jsonwebtoken';
+import { headers } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 
 const secret = process.env.SECRET_TOKEN;
 
-const authMiddleware = async (req = NextRequest,) => {
-    try {
+// export const authMiddleware = async (req = NextRequest) => {
+//     try {
+//         const headersList = headers(req);
+//         const authHeader = headersList.get('authorization')
+//         console.log(authHeader, '---------authHeader')
+//         // const authHeader = req.headers.get('authorization');
+//         // Execute this code only on the server side
+//         // const authHeader = req.headers.get('authorization');
+//         console.log('-------', authHeader, '--------------authHeader');
+//         if (!authHeader || !authHeader.startsWith('Bearer ')) {
+//             return null;
+//         }
+//         const token = authHeader.split(" ")[1];
+//         if (!token) {
+//             return null;
+//         }
+//         const decoded = jwt.verify(token, secret);
+//         const userId = decoded?.userId;
+//         const role = decoded?.role;
+//         if (!userId || !role) {
+//             return null;
+//         }
+//         req.userId = userId;
+//         req.role = role;
+//         return req;
 
-        const authHeader = req.headers.get('authorization');
-        console.log('-------',authHeader, '--------------authHeader')
+//     } catch (error) {
+//         console.error(error.message, '------error-----middleware');
+//         return NextResponse.json({ message: 'Unauthorized entry token' }, { status: 401 });
+//     }
+// };
+
+
+
+async function getHeaderData(req) {
+    const headerData = headers(req);
+    return new Promise((resolve) =>
+        setTimeout(() => {
+            resolve(headerData)
+        }, 1000)
+    )
+}
+
+export default async function authMiddleware(req = NextRequest) {
+
+    try {
+        const headerData = await getHeaderData(req)
+        const authHeader = headerData.get('authorization')
+        console.log('-------', authHeader, '------54--------authHeader');
         if (!authHeader || !authHeader.startsWith('Bearer ')) {
-            return null
+            return null;
         }
         const token = authHeader.split(" ")[1];
         if (!token) {
-            return null
+            return null;
         }
         const decoded = jwt.verify(token, secret);
         const userId = decoded?.userId;
         const role = decoded?.role;
         if (!userId || !role) {
-            return null
+            return null;
         }
-        req.userId = userId
-        req.role = role
-        return req
+        req.userId = userId;
+        req.role = role;
+        return req;
+
     } catch (error) {
         console.error(error.message, '------error-----middleware');
         return NextResponse.json({ message: 'Unauthorized entry token' }, { status: 401 });
     }
-};
 
-export default authMiddleware;
+}
--- src/app/projectManager/dashboard/page.jsx
+++ src/app/projectManager/dashboard/page.jsx
@@ -1,18 +1,11 @@
 "use client";
 import React from "react";
-import CardDataStats from "../components/CardDataStats";
-import ChartOne from "../components/ChartOne";
-import ChartTwo from "../components/ChartTwo";
-import ChartThree from "../components/ChartThree";
 
 const page = () => {
   return (
     <>
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
         DASHBOARD
-        {/* <ChartOne />
-        <ChartTwo />
-        <ChartThree/> */}
       </div>
     </>
   );
